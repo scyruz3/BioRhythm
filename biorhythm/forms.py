254fae9dbc87663d81bc6cf27b3fe7cadeac1976
Alejandro Quintana <49078813+AQJMTZ@users.noreply.github.com>
2022-03-17 17:15:56 -0600

    SG- "Added more functionatilty to la sign up"

--- biorhythm/forms.py
+++ biorhythm/forms.py
@@ -1,16 +1,14 @@
-from email.message import EmailMessage
-from tkinter import label
 from wsgiref.validate import validator
-from flask_wtf import Flaskform
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
-class SignUpForm(Flaskform):
+class SignUpForm(FlaskForm):
     username = StringField(label='Username', validators=[DataRequired(), Length(min = 3, max = 20)])
-    photo 
+    #photo 
     birthdate= StringField(label = 'DD/MM/YYYY', validators={DataRequired()})
     email = StringField(label='Email', validators={DataRequired(), Email()})
     password = PasswordField(label='Password', validators={DataRequired(), Length(min=6, max =24)})
     confirm_password = PasswordField(label= 'Confirm Password', validators={DataRequired(), EqualTo('password')})
-    sign_up = SubmitField(label='Sign up')
+    sign_up = SubmitField(label='Register now!')
