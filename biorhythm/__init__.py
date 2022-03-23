95388fd4cdcdef2c33918bc042b0636856462e58
Víctor Padrón <victorpadronolivas@gmail.com>
2022-03-23 15:08:16 -0700

    refactored layout

--- biorhythm/__init__.py
+++ biorhythm/__init__.py
@@ -3,8 +3,6 @@ import os
 from dotenv import load_dotenv
 from flask_pymongo import PyMongo
 from flask import Flask
-from flask_bootstrap import Bootstrap
-from flask_datepicker import datepicker
 
 load_dotenv()
 
@@ -16,8 +14,6 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = SESSION_SECRET
 app.config["MONGO_URI"] = MONGO_URL
 mongo = PyMongo(app)
-Bootstrap(app)
-datepicker(app=app, local=["static/js/jquery-ui.js", "static/css/jquery-ui.css"])
 
 import biorhythm.views.registerView
 import biorhythm.views.dashboardView
--- biorhythm/manager/eventManager.py
+++ biorhythm/manager/eventManager.py
@@ -6,6 +6,7 @@ import biorhythm
 from biorhythm.dao import eventDAO, userDAO
 import datetime
 
+
 def getEventsCreatedByUser(userId: ObjectId) -> List[dict]:
     createdEvents = eventDAO.getEventsCreatedByUser(userId=userId)
     return createdEvents
@@ -20,61 +21,79 @@ def getPendingEventsByUser(userId: ObjectId) -> List[dict]:
     pendingEvents = eventDAO.getPendingEventsByUser(userId=userId)
     return pendingEvents
 
+
 def getEvent(eventId: ObjectId):
-    event = eventDAO.getEventbyEventId(eventId)    
+    event = eventDAO.getEventbyEventId(eventId)
     event = {
-        'title': event['title'],
-        'description': event['description'],
-        'eventDate': str(event['eventDate'].year) + '-' + str(event['eventDate'].month) + '-' + str(event['eventDate'].day),
-        'eventTime': str(event['eventDate'].hour) + ':' + str(event['eventDate'].minute),
-        'invitedUsers': event['invitedUsers'],
-        'biorhythmType': event['biorhythmType']
+        "title": event["title"],
+        "description": event["description"],
+        "eventDate": str(event["eventDate"].year)
+        + "-"
+        + "{:02d}".format(event["eventDate"].month)
+        + "-"
+        + "{:02d}".format(event["eventDate"].day),
+        "eventTime": "{:02d}".format(event["eventDate"].hour)
+        + ":"
+        + "{:02d}".format(event["eventDate"].minute),
+        "invitedUsers": event["invitedUsers"],
+        "biorhythmType": event["biorhythmType"],
     }
+    print(event["eventDate"])
     return event
 
+
 def postEvent(newEvent):
-    eventTime = datetime.datetime.strptime(newEvent['eventTime'], '%H:%M').time()
-    eventDate = datetime.datetime.strptime(newEvent['eventDate'], '%Y-%m-%d')
-    newEvent['eventDate'] = datetime.datetime.combine(eventDate, eventTime)
+    eventTime = datetime.datetime.strptime(newEvent["eventTime"], "%H:%M").time()
+    eventDate = datetime.datetime.strptime(newEvent["eventDate"], "%Y-%m-%d")
+    newEvent["eventDate"] = datetime.datetime.combine(eventDate, eventTime)
     newEventId = eventDAO.postNewEvent(event=newEvent)
     return str(newEventId)
 
+
 def updateEvent(eventId: ObjectId, newValues):
-    eventTime = datetime.datetime.strptime(newValues['eventTime'], '%H:%M').time()
-    eventDate = datetime.datetime.strptime(newValues['eventDate'], '%Y-%m-%d')
-    newValues['eventDate'] = datetime.datetime.combine(eventDate, eventTime)
+    eventTime = datetime.datetime.strptime(newValues["eventTime"], "%H:%M").time()
+    eventDate = datetime.datetime.strptime(newValues["eventDate"], "%Y-%m-%d")
+    newValues["eventDate"] = datetime.datetime.combine(eventDate, eventTime)
     eventUpdate = eventDAO.updateEvent(eventId, newValues)
     return eventUpdate
+
 
 def uninviteFriendFromEvent(eventId: ObjectId, uninvited: str):
     event = getEvent(eventId)
     uninvited = userDAO.getUserById(userId=ObjectId(uninvited))
-    invitedList = event['invitedUsers']
-    invitedList.remove({'userId': str(uninvited['_id']), 'username': uninvited['username']})
+    invitedList = event["invitedUsers"]
+    invitedList.remove(
+        {"userId": str(uninvited["_id"]), "username": uninvited["username"]}
+    )
     eventDAO.un_inviteFriend(eventId, invitedList)
     return 0
+
 
 def inviteFriendToEvent(eventId: ObjectId, invited: str):
     event = getEvent(eventId)
     userToInvite = userDAO.getUserById(userId=ObjectId(invited))
-    invitedList = event['invitedUsers']
-    invitedList.append({'userId': str(userToInvite['_id']), 'username': userToInvite['username']})
+    invitedList = event["invitedUsers"]
+    invitedList.append(
+        {"userId": str(userToInvite["_id"]), "username": userToInvite["username"]}
+    )
     eventDAO.un_inviteFriend(eventId, invitedList)
     return 0
 
+
 def inviteAllFriends(eventId: ObjectId, allList):
     event = getEvent(eventId)
-    invitedList = event['invitedUsers']
+    invitedList = event["invitedUsers"]
     for invited in allList:
         invitedList.append(invited)
     eventDAO.un_inviteFriend(eventId, invitedList)
     return 0
 
+
 def deleteEvent(eventId: ObjectId):
     eventDAO.deleteEvent(eventId)
     return 0
 
+
 def getUsers() -> List[dict]:
     users = userDAO.getAllUsers()
     return users
-    
